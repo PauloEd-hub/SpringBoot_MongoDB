f55265d1e1e513c45dcf72890e8470b673f4cd95
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-05-16 23:47:21 -0300

    Criando primeira requisição

---
+++ src/main/java/com/paulocavalcante/workshopmongo/resources/UserResources.java
@@ -0,0 +1,31 @@
+  package com.paulocavalcante.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paulocavalcante.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		
+		User maria = new User("1", "Maria Silva", "maria@gmail.com");
+		User alex = new User("2", "Alex Silva", "alex@gmail.com");
+		
+		List<User> list = new ArrayList<>();
+		
+		list.addAll(Arrays.asList(maria, alex));
+		
+		return ResponseEntity.ok().body(list);
+	}
+
+}
