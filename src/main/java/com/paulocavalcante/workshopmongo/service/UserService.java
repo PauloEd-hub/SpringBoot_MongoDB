68404081e260a1961d7689764bcc5cc881d8957e
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-05-20 00:42:21 -0300

    Inserção de usuário com post

--- src/main/java/com/paulocavalcante/workshopmongo/service/UserService.java
+++ src/main/java/com/paulocavalcante/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paulocavalcante.workshopmongo.domain.User;
+import com.paulocavalcante.workshopmongo.dto.UserDTO;
 import com.paulocavalcante.workshopmongo.repository.UserRepository;
 import com.paulocavalcante.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -25,6 +26,14 @@ public class UserService {
 		Optional <User> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
