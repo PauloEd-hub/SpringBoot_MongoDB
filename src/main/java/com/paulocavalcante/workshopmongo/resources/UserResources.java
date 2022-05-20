5efac6ddde90aab0a5213935d71fff5c6baea62d
PauloEd-hub <pauloedu.cavalcante@gmail.com>
2022-05-20 01:00:01 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/paulocavalcante/workshopmongo/resources/UserResources.java
+++ src/main/java/com/paulocavalcante/workshopmongo/resources/UserResources.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,6 +50,14 @@ public class UserResources {
 		
 		return ResponseEntity.created(uri).build();
 	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	
 
